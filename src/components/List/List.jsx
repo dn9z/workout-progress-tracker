a112b49acd1ddb49993bc7d842200ab8eae65358
dn9z <28568855+dn9z@users.noreply.github.com>
2022-05-09 17:34:50 +0200

    fixed infinite scroll partially

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,32 +1,37 @@
-import { useState, useEffect,useRef,useCallback ,useContext } from "react";
+import { useState, useEffect, useRef, useCallback, useContext } from "react";
 import "./List.scss";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Modal from "../Modal/Modal";
+import Modal from "../utils/Modal/Modal";
 import Form from "../AddEntryForm/AddEntryForm";
 import usePaginate from "../utils/usePaginate";
 import { MyContext } from "../context/Context";
 const List = ({ activeItem, setActiveItem }) => {
   const navigate = useNavigate();
   const [showAddModal, setShowAddModal] = useState(false);
-  const {searchQueryInput, pageNumber, setPageNumber} = useContext(MyContext)
-  const data = usePaginate(searchQueryInput,pageNumber)
 
+  const { searchQueryInput, pageNumber, setPageNumber } = useContext(MyContext);
+  const data = usePaginate(searchQueryInput, pageNumber);
 
-  const observer = useRef()
-  const lastWorkoutElementRef = useCallback(node => {
-    if (data.loading) return
-    if (observer.current) observer.current.disconnect()
-    observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && data.hasMore) {
-        setPageNumber(prevPageNumber => prevPageNumber + 1)
-      }
-    })
-    if (node) observer.current.observe(node)
-  }, [data.loading, data.hasMore])
+  const observer = useRef();
+  const lastWorkoutElementRef = useCallback(
+    (node) => {
+      if (data.loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && data.hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [data.loading, data.hasMore]
+  );
 
-
-
+  function handleNavigate(ele) {
+    navigate(`/workouts/details/${ele._id}`);
+    setActiveItem(ele);
+  }
 
   return (
     <>
@@ -39,23 +44,35 @@ const List = ({ activeItem, setActiveItem }) => {
           New
         </button>
 
-          {data.workouts.map((ele,i) => {
-            if(data.workouts.length === i + 1){
-              // if last element of fetch call, add ref to element to trigger intersectionobserver when element is visible
-              return <li ref={lastWorkoutElementRef} onClick={() => {
-                navigate(`/workouts/details/${ele._id}`);
-                setActiveItem(ele);
-              }} className={activeItem && activeItem._id === ele._id ? `active` : ""} key={i}><div>{ele.type.name}</div><div>{ele.date.slice(0,10)}</div></li>
-            }else{
-              return <li key={i} onClick={() => {
-                navigate(`/workouts/details/${ele._id}`);
-                setActiveItem(ele);
-              }} className={activeItem && activeItem._id === ele._id ? `active` : ""}><div>{ele.type.name}</div><div>{ele.date.slice(0,10)}</div></li>
-            }
-          })}
-          {data.loading && <div>loading....</div>}
-          {data.error && <div>error</div>}
-
+        {data.workouts.map((ele, i) => {
+          if (data.workouts.length === i + 1) {
+            // if last element of fetch call, add ref to element to trigger intersectionobserver when element is visible
+            return (
+              <li
+                ref={lastWorkoutElementRef}
+                onClick={() => handleNavigate(ele)}
+                className={activeItem && activeItem._id === ele._id ? `active` : ""}
+                key={i}
+              >
+                <div>{ele.type.name}</div>
+                <div>{ele.date.slice(0, 10)}</div>
+              </li>
+            );
+          } else {
+            return (
+              <li
+                key={i}
+                onClick={() => handleNavigate(ele)}
+                className={activeItem && activeItem._id === ele._id ? `active` : ""}
+              >
+                <div>{ele.type.name}</div>
+                <div>{ele.date.slice(0, 10)}</div>
+              </li>
+            );
+          }
+        })}
+        {data.loading && <div>loading....</div>}
+        {data.error && <div>error</div>}
       </div>
       {/* <Outlet /> */}
       {showAddModal && (
--- src/components/utils/usePaginate.jsx
+++ src/components/utils/usePaginate.jsx
@@ -9,46 +9,87 @@ export default function usePaginate(searchQueryInput, pageNumber) {
     hasMore: false,
   });
 
-  useEffect((prev) => {
-    setPaginate({
-      ...prev,
-      workouts: [],
-    });
-  }, [searchQueryInput]);
-
-  useEffect(() => {
+  const getData = async () => {
     setPaginate({
       ...paginate,
       loading: true,
       error: false,
     });
-    let cancel;
-    axios({
-      method: "GET",
-      url: `http://localhost:9001/api/workouts/paginate`,
-      params: { searchQuery: searchQueryInput, page: pageNumber },
-      cancelToken: new axios.CancelToken((c) => (cancel = c)),
-    })
-      .then((res) => {
-        setPaginate(() => {
-          console.log(paginate.workouts)
-          return {
-            ...paginate,
-            workouts: [...paginate.workouts, ...res.data],
-            hasMore: res.data.length > 0,
-            loading: false,
-          };
-        });
-      })
-      .catch((e) => {
-        if (axios.isCancel(e)) return;
-        setPaginate({
-          ...paginate,
-          error: true,
-        });
+    // let cancel;
+    try {
+      return await axios({
+        method: "GET",
+        url: `http://localhost:9001/api/workouts/paginate`,
+        params: { searchQuery: searchQueryInput, page: pageNumber },
+        // cancelToken: new axios.CancelToken((c) => (cancel = c)),
       });
-    return () => cancel();
-  }, [searchQueryInput, pageNumber]);
+    } catch (e) {
+      // if (axios.isCancel(e)) return;
+      console.log(e)
+      setPaginate({
+        ...paginate,
+        error: true,
+      });
+    }
+  };
+
+  useEffect(() => {
+    getData().then((res) => {
+      setPaginate(() => {
+        // console.log(paginate.workouts)
+        return {
+          ...paginate,
+          workouts: [...res.data],
+          hasMore: res.data.length > 0,
+          loading: false,
+        };
+      });
+    });
+    // return () => cancel();
+  }, [searchQueryInput]);
+
+  useEffect(() => {
+    getData().then((res) => {
+      setPaginate(() => {
+        // console.log(paginate.workouts)
+        return {
+          ...paginate,
+          workouts: [...paginate.workouts, ...res.data],
+          hasMore: res.data.length > 0,
+          loading: false,
+        };
+      });
+      // return () => cancel();
+    });
+
+    // setPaginate({
+    //   ...paginate,
+    //   loading: true,
+    //   error: false,
+    // });
+    // let cancel;
+    // axios({
+    //   method: "GET",
+    //   url: `http://localhost:9001/api/workouts/paginate`,
+    //   params: { searchQuery: searchQueryInput, page: pageNumber },
+    //   cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    // })
+    //   .then((res) => {
+    //     setPaginate(() => {
+    //       // console.log(paginate.workouts)
+    //       return {
+    //         ...paginate,
+    //         workouts: [...paginate.workouts, ...res.data],
+    //         hasMore: res.data.length > 0,
+    //         loading: false,
+    //       };
+    //     });
+    //   })
+    //   .catch((e) => {
+
+    //   });
+    // return () => cancel();
+  }, [pageNumber]);
 
   return paginate;
 }
